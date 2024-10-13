7fb5c44d4634eedd6e23243a49c90faff5592e90
larissa-fernanda <laliffr@gmail.com>
2024-10-13 16:05:50 -0300

    feat: Adiciona implementação do método calculateNetSalary()

--- src/main/java/fatec/lg/SalaryCalculator.java
+++ src/main/java/fatec/lg/SalaryCalculator.java
@@ -2,7 +2,31 @@ package fatec.lg;
 
 public class SalaryCalculator {
     public double calculateNetSalary(Employee employee) {
-        return employee.getBaseSalary();
-    }
+        double baseSalary = employee.getBaseSalary();
+        Role role = employee.getRole();
 
+        switch (role) {
+            case DESENVOLVEDOR:
+                if (baseSalary >= 3000) {
+                    return baseSalary * 0.80;
+                } else {
+                    return baseSalary * 0.90;
+                }
+            case DBA:
+            case TESTADOR:
+                if (baseSalary >= 2000) {
+                    return baseSalary * 0.75;
+                } else {
+                    return baseSalary * 0.85;
+                }
+            case GERENTE:
+                if (baseSalary >= 5000) {
+                    return baseSalary * 0.70;
+                } else {
+                    return baseSalary * 0.80;
+                }
+            default:
+                throw new IllegalArgumentException("Cargo inválido");
+        }
+    }
 }
